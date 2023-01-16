2e1ab8a279b63b82c71982772a322950b58372fd
Ayunay <maya.jabs@yahoo.de>
2023-01-16 12:54:31 +0100

    Initial commit

---
+++ AvlTree/EditTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvlTree
+{
+    internal class EditTree
+    {
+        public void Add()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void Search()
+        {
+
+        }
+    }
+}
---
+++ AvlTree/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace AvlTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameFlow = true;
+
+            while (gameFlow)
+            {
+                Outsorced outsorced = new Outsorced();
+                EditTree editTree = new EditTree();
+                
+                // Menu "Do you want to play the Game or exit?"
+                gameFlow = outsorced.MenuSelector();
+
+                if (gameFlow)
+                {
+                    char edit = outsorced.HowToEditTree();
+
+                    switch (edit)
+                    {
+                        case '1':
+                            editTree.Add();
+                            break;
+
+                        case '2':
+                            editTree.Delete();
+                            break;
+
+                        case '3':
+                            editTree.Search();
+                            break;
+                    }
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
